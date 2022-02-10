3adef19e076a65c468f63120401e551b67809649
Reza Abolhasanzadeh <rezaprog@yahoo.com>
2022-02-10 16:31:43 +0330

    Add project files.

---
+++ Earth/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Earth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Earth");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("inbox",
+                false,
+                false,
+                false,
+                null);
+
+            var message = " ";
+
+            while (message != string.Empty)
+            {
+                Console.WriteLine("type Message :");
+
+                message = Console.ReadLine();
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("",
+                    "inbox",
+                    null,
+                  body);
+
+                Console.WriteLine("sent from [Earth] : " + message);
+            }
+
+        }
+    }
+}
---
+++ Moon/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Moon // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! moon");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("inbox",
+                false,
+                false,
+                false,
+                null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume("inbox",
+                true,
+                consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Console.WriteLine($"Message received from earth : " + message);
+        }
+    }
+}
